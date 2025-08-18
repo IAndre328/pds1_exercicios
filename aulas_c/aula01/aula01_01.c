73f67d24dc942ef30e32348fb4c741db3123ea03
André Batista <andrerafaelbatista@gmail.com>
2025-08-18 18:26:58 -0300

    Minhas anotações sobre a primeira aula

---
+++ aulas_c/aula01/aula01_01.c
@@ -0,0 +1,39 @@
+int main(){
+    //...
+
+    // Todo programa em C começa com essa função main.
+    // Não tenha mais de uma função com o nome main, dá ruim
+    // Tudo entre chaves está entre no scopo ("dentro") da função main
+
+    int NomeDaMinhaVariavel; // PONTO E VÍRGULA OBRIGATÓRIO;
+    // Use nomes de variáveis autoexplicativos
+
+    // int -> inteiro
+    // float -> fracionário
+    // char -> caracter
+
+    // Programas são executados de CIMA para BAIXO
+    // Não coloque nome de variáveis como palavras reservadas. Ex: int int = 0; Esse tipo de coisa dá problema
+
+    // Assinalamento ou Atribuição
+    NomeDaMinhaVariavel = 0;
+    // Estou atribuindo a minha variável a um valor.
+
+
+    // Diferentes maneiras de reservar espaço da sua variavel parte 1
+    // 1 - signed 
+    signed int variavel1 = 0;
+    // Um bit é reservado para o sinal de + ou -
+
+    // 2 - unsigned
+    unsigned int variavel2 = 0;
+    // Use somente para números acima de 0, cabe o dobro para mais
+
+    // 3 - short
+    short int variavel3 = 0;
+    // Reserva somente 8 bits -> 1 byte para essa variével
+
+
+
+    //...
+}
